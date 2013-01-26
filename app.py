b7b9917b3aed1ea57314c853c2fa80178c204bc6
oliversong <oliversong@tumblr.com>
2013-01-26 17:53:18 -0500

    Could it be a gunicorn problem

--- app.py
+++ app.py
@@ -26,16 +26,16 @@ SECRET_KEY = "devopsborat"
 apiKey = "qSoNop1JjHxPQcJkv3L5rrmgBrqNgC1t"
 
 # local
-#pad = EtherpadLiteClient(apiKey,'http://0.0.0.0:9001/api')
+pad = EtherpadLiteClient(apiKey,'http://0.0.0.0:9001/api')
 # remote
-pad = EtherpadLiteClient(apiKey,'http://goombastomp.cloudfoundry.com/api')
+#pad = EtherpadLiteClient(apiKey,'http://goombastomp.cloudfoundry.com/api')
 
 # make app
 app = Flask(__name__)
 #heroku
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #local
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/rocketjumpdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/rocketjumpdb'
 
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
