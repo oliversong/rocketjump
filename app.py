6007190b1fc043ececafd826f8a92e1e18cc5a78
oliversong <oliversong@tumblr.com>
2013-01-26 18:50:24 -0500

    this is stupid

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ from flask_oauth import OAuth, OAuthException
 from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 from py_etherpad import EtherpadLiteClient
+from flask.ext.social import Social
+from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
 
 # config
 DEBUG = True
@@ -23,8 +27,11 @@ PER_PAGE = 20
 SECRET_KEY = "devopsborat"
 
 # facebook api connection
-FACEBOOK_APP_ID = '124499577716801'
-FACEBOOK_APP_SECRET = '8f3dc21d612f5ef19dbc98221e1c7a0d'
+app.config['SOCIAL_FACEBOOK'] = {
+    'consumer_key': '124499577716801',
+    'consumer_secret': '8f3dc21d612f5ef19dbc98221e1c7a0d'
+}
+app.config['SECURITY_POST_LOGIN'] = '/home'
 
 # etherpad api connection
 apiKey = "qSoNop1JjHxPQcJkv3L5rrmgBrqNgC1t"
@@ -41,6 +48,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/rocketjumpdb'
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
+app.SERVER_NAME = 'notability.org'
 oauth = OAuth()
 db = SQLAlchemy(app)
 app.config.from_object(__name__)
@@ -94,7 +102,16 @@ queueTable = db.Table('queueTable',
     db.Column('user_id', db.BigInteger, db.ForeignKey('users.id'))
 )
 
-class User(db.Model):
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     fid = db.Column(db.BigInteger, unique=True)
@@ -110,6 +127,8 @@ class User(db.Model):
     authorID = db.Column(db.String(120), unique=True)
     sessionID = db.Column(db.String(120))
     college = db.Column(db.String(120))
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
     def __init__(self, fid, fname, lname, email, username, college):
         self.fid = fid
@@ -217,6 +236,18 @@ class Asset(db.Model):
     def __repr__(self):
         return '<Asset %r>' % self.location
 
+class Connection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    provider_id = db.Column(db.String(255))
+    provider_user_id = db.Column(db.String(255))
+    access_token = db.Column(db.String(255))
+    secret = db.Column(db.String(255))
+    display_name = db.Column(db.String(255))
+    profile_url = db.Column(db.String(512))
+    image_url = db.Column(db.String(512))
+    rank = db.Column(db.Integer)
+
 def matchmake(lecture):
 
     # SHIIIEEET BLACK BOX ALGORITHM BRO
@@ -280,6 +311,10 @@ def before_request():
 ###
 # Routing for your application.
 ###
+
+Security(app, SQLAlchemyUserDatastore(db, User, Role))
+Social(app, SQLAlchemyConnectionDatastore(db, Connection))
+
 '''
 @app.errorhandler(OAuthException)
 def handle_oauth_exception(error):
