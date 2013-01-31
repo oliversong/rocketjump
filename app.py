9da9076e32554533c48b4149acbd0c6e588e7113
oliversong <oliversong@tumblr.com>
2013-01-31 04:07:05 -0500

    Oops not displaying time correctly in notepad

--- app.py
+++ app.py
@@ -327,7 +327,7 @@ class ItsdangerousSessionInterface(SessionInterface):
 
 class EST(tzinfo):
     def utcoffset(self, dt):
-      return timedelta(hours=-5)
+        return timedelta(hours=-5)
 
     def dst(self, dt):
         return timedelta(0)
